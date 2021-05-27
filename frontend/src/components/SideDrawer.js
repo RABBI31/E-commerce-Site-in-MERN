16385476b6bd14596844193b768e845dfab138fb
Rabbi <alirabbi3112@gmail.com>
2021-05-28 00:08:54 +0600

    Navbar and Showdrawer added

---
+++ frontend/src/components/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SideDrawer.css'
+
+function SideDrawer() {
+    return (
+        <div className='sidedrawer'>
+            
+        </div>
+    )
+}
+
+export default SideDrawer
